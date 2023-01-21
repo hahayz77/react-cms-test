87939f238194f440abf2e32fca8402cb612d696c
hahayz77 <danilo__danilo@hotmail.com>
2023-01-21 09:45:56 -0300

    formData success send

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
 import './App.css';
 
 
 export function App() {
-    
+
+    // INIT #################################
     const [images, setImages] = useState([]);
+    const [loadingImg, setLoadingImg] = useState(false);
+    
+    // GET #################################
     
     useEffect(() => {getApiData()}, []);
     
     const getApiData = async () => {
         const response = await fetch(
-          "http://localhost:3000"
-        ).then((response) => response.json());
-      
-        // update the state
-        setImages(response);
-      };
+            "http://localhost:3000"
+            ).then((response) => response.json());
+            setImages(response);
+        };
+        
+        // POST #################################  
+        const { register, handleSubmit } = useForm();
+
+    const onSubmit = async (data) => {
+        if(!data){
+            console.log("No imgs to upload!");
+            return;
+        }
+        setLoadingImg(true);
+        const formData = new FormData();
+        formData.append("file", data.file[0]);
+
+        const response = await fetch("http://localhost:3000/post", {
+            method: "POST",
+            body: formData,
+        })
+        .then(() => console.log(response))
+        .catch((error) => {
+            setLoadingImg(false);
+            console.log(error)
+        });
+        await getApiData();
+        setLoadingImg(false);
+    };
+
+    // DELETE #################################
     
 
     return (
-        <div className="App">
+        <div className='App'>
             <h1>S3 Bucket</h1>
+            <div className='inputFile'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="file" {...register("file")} />
+                <input type="submit" />
+            </form>
+            </div>
+            { loadingImg === true && <div className='loading'>Loading ...</div> }
             {images.map((imgs, index)=>{return (
                 <>
                 <div className='container'>
@@ -36,6 +74,8 @@ export function App() {
                 <hr/>
                 </>
             )})}
+
+            {images.length === 0 && <h3>Nothing here!</h3>}
         </div>
     );
 }
